61678260b41c7de90a85dd0283e59b494e2bbca8
Drew Miley <drew.miley@opencastsoftware.com>
2017-03-17 16:27:43 +0000

    Displays correct todo name and number

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
