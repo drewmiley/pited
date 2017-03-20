bf9fd0de10a8357f598935a08679553e80121b27
Drew Miley <drew.miley@opencastsoftware.com>
2017-03-20 14:01:26 +0000

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -39,6 +39,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists '1: Buy peacock feathers' as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(0.2)
+
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1 : Buy peacock feathers')
@@ -69,6 +70,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(0.2)
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
-from lists.models import Item
+from .models import Item
 
 @csrf_exempt
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+@csrf_exempt
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
